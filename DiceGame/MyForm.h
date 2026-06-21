77db0e69f449f5eaf4c2620c468ce84f90c93344
kurase <kojikurase@withe.ne.jp>
2026-06-21 20:00:12 +0900

    0x000001f4c6d960781

--- DiceGame/MyForm.h
+++ DiceGame/MyForm.h
@@ -285,7 +285,7 @@ namespace DiceGame {
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 
-		CString xx="dc";
+		CString xx="dc2";
 
 		if (this->radioButtonKisu->Checked == true)
 		{
